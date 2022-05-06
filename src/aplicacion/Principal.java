e7ac1d0975bb629d31ca2cc00630ff3d55d32611
Danii <danielfernandezlopez75@gmail.com>
2022-05-06 10:32:23 +0200

    HOLA

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,11 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.*;
+
+import java.util.Arrays;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(TablaFunciones.crearTabla(10, 10 )));
+        System.out.println("El mayor es: " + TablaFunciones.mayor(TablaFunciones.crearTabla(10, 10 )));
+    }
 }
--- src/dominio/TablaFunciones.java
+++ src/dominio/TablaFunciones.java
@@ -1,2 +1,30 @@
-package dominio;public class TablaFunciones {
+package dominio;
+
+public class TablaFunciones {
+
+    //crear una tabla de 10x10 con numeros aleatorios por argumentos
+
+    public static int[][] crearTabla(int filas, int columnas) {
+        int[][] tabla = new int[filas][columnas];
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                tabla[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        return tabla;
+    }
+
+// funcion que devuelva el mayor valor de la tabla
+    public static int mayor(int[][] tabla) {
+        int mayor = 0;
+        for (int i = 0; i < tabla.length; i++) {
+            for (int j = 0; j < tabla[i].length; j++) {
+                if (tabla[i][j] > mayor) {
+                    mayor = tabla[i][j];
+
+                }
+            }
+        }
+        return mayor;
+    }
 }
